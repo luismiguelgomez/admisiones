03c32cc7cf0480f0ff9c1e151d12106283d287a3
luismiguelgomez <gomezpaezluismiguel@gmail.com>
2021-07-21 13:16:27 -0500

    Inicio del front

---
+++ database/migrations/2021_07_16_145805_create_registros_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->id();
+            $table->String('nombre');
+            $table->String('ciudad');
+            $table->String('telefono');
+            $table->String('apellidos');
+            $table->String('email');
+            $table->String('grado');
+            $table->integer('dia');
+            $table->integer('mes');
+            $table->integer('anio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros');
+    }
+}
