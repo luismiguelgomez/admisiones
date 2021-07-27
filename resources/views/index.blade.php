7eaec0d12b53767b214fff43d59c821ef3bba25c
Christian David Becerra Montoya <cdavidbm@gmail.com>
2021-07-27 13:04:28 -0500

    Ventana modal en formulario. Formulario terminado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,8 @@
   <title>DIRECTORIO FAMILIA UNIAGUSTINIANA EN TIEMPOS DE CRISIS</title>
 
   <!-- Bootstrap core CSS -->
-  <!--<script src="{{ asset('js/agency.min.js') }}"></script>-->
-  <link href="{{ asset('plantilla/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">  
+
 
   <!-- Custom fonts for this template -->
   <link href="{{ asset('plantilla/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
@@ -46,10 +46,10 @@
             <div class="col-md-12">
                 <div class="row">
 
-                    <div class="col-md-5 offset-7" style="background: gray; opacity: 0.92 ; border: 1px solid black; border-radius: 15px 15px 15px 15px;">
+                <div class="col-md-5" style="background: #0606068a; opacity: 0.9;  border: 1px solid black; border-radius: 15px 15px 15px 15px;">
 
-                        <h3 align="center" style="color: black;"> <strong> Registra tus Datos </strong> </h2>
-                        <h4 align="center" style="color: black;">Se parte de la familia Agustiniana</h3>
+                        <h3 align="center" style="color: white;"> <strong> Registra tus Datos </strong> </h2>
+                        <h4 align="center" style="color: white;">Se parte de la familia Agustiniana</h3>
 
                           <form action="formulario" method="POST" enctype="multipart/form-data">
                           @csrf
@@ -79,9 +79,44 @@
                                 <option value="once">Once</option>
                             </select>
                             <br>
-                            <button type="submit" class="btn btn-dark btn-block">Enviar</button>
+                            <button type="submit" class="btn btn-primary btn-block">Enviar</button>
                             <br>
                                 <span align="center">  * Autorizo el uso de datos aqui registrados </span>
+
+
+
+<!-- Ventana modal -->
+
+    <!-- Boton -->               
+    <br>
+<a style="color: orange; font-size: 0.9em;" data-toggle="modal" data-target="#exampleModal">
+Ver la Política de Tratamiento de la Información
+</a>
+     
+
+    <!-- Modal -->
+    <div class="modal fade" data-backdrop="false" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+      <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content" style="height: 600px; width: 800px;">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Política de Tratamiento de la Información</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <iframe width="100%" height="100%" src="https://agustinianonorte.edu.co/wp-content/uploads/2020/02/POLITICA-DE-PRIVACIDAD-DATOS-PERSONALES-COLEGIOS.pdf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+
+                    
                             <br><br>
                           </form>
                     </div>
@@ -91,6 +126,11 @@
       </div>
     </section>
   </header>
+
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   
 </body>
 </html>
